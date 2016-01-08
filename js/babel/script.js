0e83afcc5405a706430a033856c430aaa2802eb9
Bartosz Wieczorek <hello.bartosz@gmail.com>
2016-01-08 18:30:17 +0100

    editi script and content of 'progressive images'

--- js/babel/script.js
+++ js/babel/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 (function () {
 
@@ -13,46 +13,51 @@
   // so 260 threshold will fine
   var imgThreshold = 260;
 
-  console.log("img 0 threshold:" + imgAll[0].clientHeight);
-  console.log("img 1 threshold:" + imgAll[1].clientHeight);
+  // setTiemout sets acceptable delay of rendering process
+  setTimeout(function () {
 
-  // if there's only one image
-  if (imgAll[0] !== undefined && imgAll[1] === undefined) {
+    // if there's only one image
+    if (imgAll[0] !== undefined && imgAll[1] === undefined) {
 
-    // if it's not loading fast enough,
-    // add proper blur animation
-    if (imgAll[0].clientHeight < imgThreshold) {
-
-      imgAll[0].classList.add('img-blur-1');
-      console.log('img 0: blur 1');
-    }
-  }
-
-  // if there are two or more images, add proper blur animations
-  else if (imgAll[1] !== undefined) {
-
-      // when only first image is loading fast enough
-      if (imgAll[0].clientHeight > imgThreshold && imgAll[1].clientHeight < imgThreshold) {
+      console.log("img 0 threshold: " + imgAll[0].clientHeight);
+      // if it's not loading fast enough,
+      // add proper blur animation
+      if (imgAll[0].clientHeight < imgThreshold) {
 
         imgAll[0].classList.add('img-blur-1');
         console.log('img 0: blur 1');
-
-        for (var i = 1; i < imgAll.length; i += 1) {
-          imgAll[i].classList.add('img-blur-2');
-        }
-        console.log('img rest: blur 2');
       }
+    }
 
-      // when all images are not loading fast enough
-      else if (imgAll[0].clientHeight < imgThreshold && imgAll[1].clientHeight < imgThreshold) {
+    // if there are two or more images, add proper blur animations
+    else if (imgAll[1] !== undefined) {
 
-          imgAll[0].classList.add('img-blur-2');
-          console.log('img 0: blur 2');
+        // when only first image is loading fast enough
+        if (imgAll[0].clientHeight > imgThreshold && imgAll[1].clientHeight < imgThreshold) {
+
+          console.log("img 0 threshold: " + imgAll[0].clientHeight);
+          console.log("img 1 threshold: " + imgAll[1].clientHeight);
+
+          imgAll[0].classList.add('img-blur-1');
+          console.log('img 0: blur 1');
 
           for (var i = 1; i < imgAll.length; i += 1) {
-            imgAll[i].classList.add('img-blur-3');
+            imgAll[i].classList.add('img-blur-2');
           }
-          console.log('img rest: blur 3');
+          console.log('img rest: blur 2');
         }
-    }
+
+        // when all images are not loading fast enough
+        else if (imgAll[0].clientHeight < imgThreshold && imgAll[1].clientHeight < imgThreshold) {
+
+            imgAll[0].classList.add('img-blur-2');
+            console.log('img 0: blur 2');
+
+            for (var i = 1; i < imgAll.length; i += 1) {
+              imgAll[i].classList.add('img-blur-3');
+            }
+            console.log('img rest: blur 3');
+          }
+      }
+  }, 100);
 })();
