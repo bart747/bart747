3adcb2ee22a6fb8355543af96d62ddedce473365
Bartosz Wieczorek <hello.bartosz@gmail.com>
2016-02-13 00:04:46 +0100

    add coffeescript support (gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,13 @@ var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var coffee = require('gulp-coffee');
+var coffeelint = require('gulp-coffeelint');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
 var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
+var gutil = require('gulp-util');
 
         
 // sass => css
@@ -31,9 +34,19 @@ gulp.task('minify-css', ['sass-css'], function() {
     .pipe(gulp.dest('css/dist'));
 });
 
+// coffeescript 
+gulp.task('coffee', function() {
+  return gulp.src('./js/myscripts/coffee/*.coffee')
+    .pipe(sourcemaps.init()
+      .pipe(coffeelint())
+      .pipe(coffeelint.reporter()))
+      .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./js/myscripts/coffee'));
+});
 
-// scripts 
-gulp.task('scripts', function() {
+// js
+gulp.task('js', function() {
   return gulp.src('js/myscripts/*.js')
     .pipe(sourcemaps.init())
       .pipe(jshint({
@@ -46,8 +59,8 @@ gulp.task('scripts', function() {
 });
 
 // script compression
-gulp.task('compress', ['scripts'], function() {
-  return gulp.src(['js/plugins/*.js', './js/babel/*.js'])
+gulp.task('compress', ['js'], function() {
+  return gulp.src(['js/plugins/*.js', './js/babel/*.js', './js/myscripts/coffee/*.js'])
     .pipe(sourcemaps.init())
       .pipe(uglify())  
       .pipe(concat('all.js'))
@@ -66,15 +79,18 @@ gulp.task('serve', function() {
 
   gulp.watch('./_scss/*.scss', ['sass-css', 'minify-css'])
     .on('change', browserSync.reload);
-  gulp.watch('./js/myscripts/*.js', ['scripts', 'compress'])
+  gulp.watch('./js/myscripts/coffee/*.coffee', ['coffee'])
+    .on('change', browserSync.reload);
+  gulp.watch('./js/myscripts/*.js', ['js', 'compress'])
     .on('change', browserSync.reload);
 });
 
 // watch
 gulp.task('watch', function() {
   gulp.watch('./_sass/*.scss', ['sass-css', 'minify-css']);
-  gulp.watch('./js/myscripts/*.js', ['scripts', 'compress']);
+  gulp.watch('./js/myscripts/coffee/*.coffee', ['coffee']);
+  gulp.watch('./js/myscripts/*.js', ['js', 'compress']);
 });
 
 // default
-gulp.task('default', ['serve']);
+gulp.task('default', ['watch']);
