2e2be3e64f7ede6c7dc2f343d665323670b7fdde
Bartosz Wieczorek <hello.bartosz@gmail.com>
2016-03-09 17:31:21 +0100

    initial: new project: music for coders

---
+++ projects/music/js/babel/myScript.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// FitVIdz settings
+$(document).ready(function () {
+  // Target your .container, .wrapper, .post, etc.
+  $(".container-video").fitVids();
+});
---
+++ projects/music/js/myScript.js
@@ -0,0 +1,7 @@
+
+// FitVIdz settings
+$(document).ready(function(){
+  // Target your .container, .wrapper, .post, etc.
+  $(".container-video").fitVids();
+});
+
