7015ac54c76c29caa1f4679b051dec1b0808b4f8
Bartosz Wieczorek <hello.bartosz@gmail.com>
2016-09-02 03:58:56 +0200

    add regex validation to homepage forms
    also prevent buttons from submiting

--- js/my/togglemenu.js
+++ js/my/togglemenu.js
@@ -5,10 +5,23 @@ const toggleMenu = []
                    .slice
                    .call(doc.getElementsByClassName('menu-toggle'));
 
+const btnNonSubmit = []
+                     .slice
+                     .call(doc.getElementsByClassName('btn-prevent'));
+
+if (btnNonSubmit[0]) { 
+  btnNonSubmit.forEach( el => {
+    el.addEventListener('click', _=> {
+      return false;
+    });
+  })
+}
+
 toggleMenu.forEach( el => {
 
   let btn = el.getElementsByClassName('menu-toggle-btn');
   let content = el.getElementsByClassName('menu-toggle-content');
+
   btn[0].addEventListener('click', _ => {
     btn[0].classList.toggle('toggle-is-on');
     content[0].classList.toggle('hidden');
