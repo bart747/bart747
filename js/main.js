bf2edaa0f12d7b27f28129a93730bb6f683be957
Bartosz Wieczorek <hello.bartosz@gmail.com>
2016-09-04 18:57:23 +0200

    improve form live validation add icon support improve keyup and focusout event listeners

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const switchGeneral = require('./my/switch.js');
 const toggleMenu = require('./my/togglemenu.js');
 const formCheck = require('./my/formcheck.js');
+const formSw = require('./my/formswitch.js');
 const linkStates = require('./my/linkstates.js');
 const paymentPlugin = require('./plugins/jquery.payment.js');
 const formsCommerce = require('./my/commerce.js');
--- js/my/formcheck.js
+++ js/my/formcheck.js
@@ -1,31 +1,57 @@
 (function() {
 
-const inputs = []
+const inputDivs = []
               .slice
               .call(document.getElementsByClassName("validate"));
 
-if (inputs[0]) {
+function onKeyupValid(container, input) {
+  input.addEventListener('keyup', _=> {
+    if (input.validity.valid) {
+      container.classList.remove('input-error');
+      container.classList.add('input-success');
+    } else {
+      container.classList.add('input-error');
+      container.classList.remove('input-success');       
+    }
+  });
+}
 
-  inputs.forEach(el => {
-     
-    el.addEventListener('focusout', _=> {
-      if (el.validity.patternMismatch) {
+if (inputDivs[0]) {
 
-        el.classList.add('input-error');
+  inputDivs.forEach(el => {
+    let input = el.getElementsByTagName("input")[0];
 
+    if (input) {
+
+      input.addEventListener('focusout', _=> {
+
+        onKeyupValid(el, input);
+
+        if (input.validity.patternMismatch) {
+
+          el.classList.add('input-error');
+          el.classList.remove('input-success');
+
+        } if (input.validity.valid) {
+          el.classList.add('input-success');
+
+        } if (input.validity.valid 
+              && el.classList.contains('input-error')) {
+          el.classList.remove('input-error');
+          el.classList.add('input-success');
+        }
+      });
+
+      if (input.classList.contains("input-password")) {
         el.addEventListener('keyup', _=> {
-          if (el.validity.valid) {
+          if (input.value.length >= 8) {
+            el.classList.add('input-success');
             el.classList.remove('input-error');
-          } else {
-            el.classList.add('input-error');       
+            onKeyupValid(el, input);  
           }
         });
-
-      } else if (el.validity.valid 
-                 && el.classList.contains('input-error') ) {
-        el.classList.remove('input-error');
       }
-    });
+    }
   });
 }
 
