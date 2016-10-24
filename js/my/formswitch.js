0998975d9b5135d6c4121bf47fb6064be1546db9
Bartosz Wieczorek <hello.bartosz@gmail.com>
2016-10-24 21:13:44 +0200

    add flip animation to forms

--- js/my/formswitch.js
+++ js/my/formswitch.js
@@ -12,20 +12,28 @@ if (formsToggle[0]) {
     let btn = el.getElementsByClassName('form-switch');
     let formSignIn = el.getElementsByClassName('form-sign-in');
     let formSignUp = el.getElementsByClassName('form-sign-up');
+    let formSignInFlip = el.getElementsByClassName('form-sign-in-flip');
+    let formSignUpFlip = el.getElementsByClassName('form-sign-up-flip');
+    let flipper = el.getElementsByClassName('flipper');
     
-    function hideShow() {
-      formSignIn[0].classList.toggle('hidden');
-      formSignUp[0].classList.toggle('hidden');
+    function transform() {
+      if (formSignIn[0]) {
+        formSignIn[0].classList.toggle('hidden');
+        formSignUp[0].classList.toggle('hidden');
+      }
+      if (flipper[0]) {
+        flipper[0].classList.toggle('rotate');
+      }
     }
 
     btn[0].addEventListener('click', _ => {
       event.preventDefault();
-      hideShow();
+      transform();
     });
 
     btn[1].addEventListener('click', _ => {
       event.preventDefault();
-      hideShow();
+      transform();
     });
 
   });
